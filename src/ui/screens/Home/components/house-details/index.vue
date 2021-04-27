3a089403d39db88fa5cf95d7339c8bec6779e9ea
MHMALEK <mhos.malek@gmail.com>
2021-04-27 11:33:57 +0200

    feat: update readm

--- src/ui/screens/Home/components/house-details/index.vue
+++ src/ui/screens/Home/components/house-details/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mb-5 mt-5 bg-white sm:rounded-lg shadow-md bg-gray-100">
+  <div class="mb-5 mt-5 bg-white sm:rounded-lg">
     <div class="px-4 py-5 sm:px-6">
       <h3 class="text-lg leading-6 font-medium text-gray-900 text-left">
         {{ title }}
